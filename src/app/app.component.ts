59aa545c1e867ba28a8adc3f9c5ce62142479235
Christoph Hoeller <c.hoeller@beyond-interfaces.com>
2021-11-12 16:59:20 +0100

    ng12

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {DOCUMENT} from '@angular/common';
 
 @Component({
-  selector: 'app-root',
+  selector: 'bw-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'babywatch';
+
+  constructor(@Inject(DOCUMENT) private document: Document) {
+  }
+
+  toggleTheme() {
+    this.document.body.classList.toggle('dark-theme');
+  }
 }
